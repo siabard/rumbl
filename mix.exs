162da63ff24765f26c1ac4b83dd8288eb45dc1db
Yeonho Jang <siabard@gmail.com>
2022-10-27 16:43:22 +0900

    remove gettext

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Rumbl.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: [] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -46,7 +46,6 @@ defmodule Rumbl.MixProject do
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"}
     ]
