060a4e98095a30e97a0b095baeeb07d5ef0f0632
yhjang <siabard@gmail.com>
2022-10-28 22:37:10 +0900

    검색 함수 추가

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -16,4 +16,12 @@ defmodule Rumbl.Accounts do
   def get_user(id) do
     Enum.find(list_users(), fn map -> map.id == id end)
   end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} ->
+        Map.get(map, key) == val
+      end)
+    end)
+  end
 end
